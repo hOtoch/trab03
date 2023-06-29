273d746687844ce0de87e7831e9d650e9ab9ae08
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2023-06-28 22:31:23 -0300

    criação da função busca binária

--- utils.c
+++ utils.c
@@ -19,3 +19,19 @@ void removeNewLine(char* str) {
     }
 }
 
+int binarySearch(String** arr, int left, int right, char* key) {
+    while (left <= right) {
+        int mid = left + ((right - left) / 2);
+        int cmp = strcmp(key, getString(arr[mid]));
+
+        if (cmp == 0)
+            return mid;
+        else if (cmp < 0)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return -1;
+}
+
