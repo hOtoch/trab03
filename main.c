3cc906e4306bcddca513e87371006c82fd642ca4
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2023-06-29 20:35:48 -0300

    troquei strdups para malloc

--- main.c
+++ main.c
@@ -15,11 +15,16 @@ int main(int argc, char* argv[]){
     int swCount = 0,maxSwCount = 1000;
     int *auxSwCount = &swCount;
     char* pathStopWords = malloc(sizeof(char) * (strlen(dir) + strlen("/stopwords.txt") + 1));
-    strcpy(pathStopWords, strcat(dir,"/stopwords.txt"));
+    strcpy(pathStopWords, dir);
+    strcat(pathStopWords, "/stopwords.txt");
     String* pathStopWordsString = createString(pathStopWords);
     String** stopWordsList;
+
+
+   //printf("%s\n", getString(pathStopWordsString));
     
     stopWordsList = leArquivo(pathStopWordsString, maxSwCount, auxSwCount);
+    //printf("%d\n", swCount);
 
     qsort(stopWordsList, swCount, sizeof(String*), compareQs);
 
@@ -30,30 +35,32 @@ int main(int argc, char* argv[]){
     
     // ----- Leitura do arquivo Index -----
 
-    char* pathIndex = strcat(strdup(dir),"/index.txt");
-    
+    char* pathIndex = malloc(sizeof(char) * (strlen(dir) + strlen("/index.txt") + 1));
+    strcpy(pathIndex, dir);
+    strcat(pathIndex, "/index.txt");
     String* pathIndexString = createString(pathIndex);
-
     int maxPagesCount = 50, countPages = 0;
     int *auxCountPages = &countPages;
     String** indexList;
 
     indexList = leArquivo(pathIndexString, maxPagesCount, auxCountPages);
+    //printf("%d\n", countPages);
 
 
     
     // ----- Leitura do graph.txt e criação das paginas -----
    
-    char* dirAux = strdup(dir); // dir sendo alterado dentro da função createPages, verificar depois
-    // printf("%s\n",dirAux);
-    char* pathGraph = strcat(strdup(dir),"/graph.txt");
-    
+    char* dirAux = malloc(sizeof(char) * (strlen(dir) + strlen("/pages") + 1));
+    strcpy(dirAux, dir);
+    char* pathGraph = malloc(sizeof(char) * (strlen(dir) + strlen("/graph.txt") + 1));
+    strcpy(pathGraph, dir);
+    strcat(pathGraph, "/graph.txt");
 
-    Page** pages = createPages(pathGraph,countPages);
-    
-    // for(int i = 0; i < countPages; i++){
+    Page** pages = createPages(pathGraph, countPages);
+
+    // for (int i = 0; i < countPages; i++) {
     //     printf("%s : ", getNome(pages[i]));
-    //     for(int j = 0; j < getCountLinks(pages[i]); j++){
+    //     for (int j = 0; j < getCountLinks(pages[i]); j++) {
     //         printf("%s ", getString(getLinks(pages[i])[j]));
     //     }
     //     printf("\n");
@@ -69,14 +76,13 @@ int main(int argc, char* argv[]){
     char* termo;
     RBT* root = NULL;
 
-
     for(int i = 0; i < countPages; i++){
         strcpy(result, pathPage);
         strcat(result, getString(indexList[i])); /* Definindo o caminho dos arquivos */
         
         removeNewLine(result);
 
-        // printf("%s\n", result);
+        //printf("entrou no for %s\n", result);
         FILE* filePage = fopen(result, "r");
 
         if(verificaArquivo(filePage)){
@@ -84,16 +90,19 @@ int main(int argc, char* argv[]){
                 getline(&line, &len, filePage);
                 termo = strtok(line, " ");
                 while(termo != NULL){
-                    printf("%s\n", termo);
 
                     // Verificar se eh stopword (busca binaria)
                     int index = binarySearch(stopWordsList, 0, swCount, termo);
+                    printf("index: %d\n", index);
                     if(index != -1){
+                        printf("entrou no if\n");
                         termo = strtok(NULL, " ");
-                        printf("StopWord: %s\n", termo);
                         continue;
+                        printf("entrou no if\n");
                     }
-                    root = RBT_insert(root, createString(termo), pages[i]);
+                    printf("saiu do if\n");
+                    root = RBT_insert(root, createString(termo), pages[i]);// Nao ta inserindo certo
+                    printf("inseriu\n");
                     
                     termo = strtok(NULL, " ");
 
@@ -105,12 +114,34 @@ int main(int argc, char* argv[]){
             exit(1); 
         }
 
-        printRBT(root);
+        //printRBT(root);
 
         break;
     }
 
+    //Liberação de memória -----
+    for (int i = 0; i < swCount; i++) {
+        freeString(stopWordsList[i]);
+    }
+    free(stopWordsList);
 
+    // for (int i = 0; i < countPages; i++) {
+    //     freePage(pages[i]);
+    // }
+    // free(pages);
+
+    // for (int i = 0; i < countPages; i++) {
+    //     freeString(indexList[i]);
+    // }
+    // free(indexList);
+
+    free(dir);
+    free(pathStopWords);
+    freeString(pathStopWordsString);
+    free(pathIndex);
+    freeString(pathIndexString);
+    free(dirAux);
+    free(line);
 
     
 
