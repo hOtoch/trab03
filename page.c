782c08e28df1d72f607a8b1c770eda6933e806e4
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2023-06-29 20:36:48 -0300

    mudanças para trocar pra malloc

--- page.c
+++ page.c
@@ -8,46 +8,44 @@ struct page {
     String** links;
 };
 
-Page** createPages(char* path,int countPages){
-    char* line = (char*) malloc(sizeof(char)*100);
+Page** createPages(char* path, int countPages) {
+    char* line = (char*) malloc(sizeof(char) * 100);
     size_t len = 0;
     removeNewLine(path);
     FILE* graph = fopen(path, "r");
-    
 
     char* token;
-    char* nome;
+    char* nome = (char*) malloc(sizeof(char) * 100);  // Allocate memory for nome
+
     int countLinks;
 
-    Page** pages = (Page**)malloc(sizeof(Page*)*countPages);
+    Page** pages = (Page**) malloc(sizeof(Page*) * countPages);
     int i = 0;
-    
-    for(i = 0; i < countPages; i++){
+
+    for (i = 0; i < countPages; i++) {
         getline(&line, &len, graph);
-        
-        
+
         token = strtok(line, " ");
         strcpy(nome, token);
-      
+
         token = strtok(NULL, " ");
         countLinks = atoi(token);
         token = strtok(NULL, " ");
 
-        // printf("%s - countlinks: %d : ", nome, countLinks);
-        
         Page* page = newPage(nome, countLinks);
-        
-        for(int j = 0; j < countLinks; j++){
+
+        for (int j = 0; j < countLinks; j++) {
             page->links[j] = createString(token);
             token = strtok(NULL, " ");
         }
         pages[i] = page;
-        // printf("i: %d ; nome 0: %s\n", i, getNome(pages[i]));
     }
     fclose(graph);
+    free(nome);  // Free the allocated memory for nome
+    free(line);
     return pages;
-
 }
+
 
 char* getNome(Page* page){
     return page->nome;
