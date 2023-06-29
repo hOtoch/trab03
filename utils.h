820ce296e2f780ca351f52c470bfc6740b46a1d2
KarlaSancio <57644122+KarlaSancio@users.noreply.github.com>
2023-06-28 22:31:48 -0300

    busca binaria

--- utils.h
+++ utils.h
@@ -2,9 +2,14 @@
 #define UTILS_H_
 #include <string.h>
 
+#include "file.h"
+#include "string.h"
+
 int verifyStopWord(char* word, char** stopWordsList, int countStopWords);
 
 void removeNewLine(char* str);
+
+int binarySearch(String** arr, int left, int right, char* key);
    
 
 #endif
