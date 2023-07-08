ac47c9347ab7fa5ab44dec9e4a1095ce637a70c5
Joao Gabriel de Barros Rocha <38754105+JoaoGBarros@users.noreply.github.com>
2023-07-08 17:25:29 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -11,7 +11,11 @@ int binarySearch(String** arr, int left, int right, char* key);
 
 void searchAndPrint(TST* indexTST);
 
+TST* searchAndCalculatePR(TST* pages);
+
 TST* searchAndIndex(TST * indexTST, TST *pages, char *pathPage, String **stopWordsList, int swCount);
+
+TST* calculatePageRank(TST* pages);
 
 TST* indexador(TST* indexTST, Page *page, char *pathPage, String **stopWordsList, int swCount);
 #endif
